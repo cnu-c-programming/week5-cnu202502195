ebc24f57037acf39d600f679f1d4e82611948f9b
cnu202502195 <202502195@o.cnu.ac.kr>
2026-04-03 10:12:17 +0900

    2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -7,7 +7,7 @@ int main() {
     int size = sizeof(alphas) / sizeof(alphas[0]);
 
     for (int i = 0; i < size; i++) {
-        prinf("%s\n", alphas[i]);
+        printf("%s\n", alphas[i]);
     }
     
     return 0;
